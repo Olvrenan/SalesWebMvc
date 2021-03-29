429c193d19956c66c0fea780eb623f176efd44d1
Olvrenan <oliveirarenan339@gmail.com>
2021-03-29 16:33:44 -0300

    atualizacao de repositorio

---
+++ SalesWebMvc/Migrations/20210321171157_DepartmentForeignKey.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class DepartmentForeignKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seller_Department_Departmentid",
+                table: "Seller");
+
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "Seller",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "email",
+                table: "Seller",
+                newName: "Email");
+
+            migrationBuilder.RenameColumn(
+                name: "birthDay",
+                table: "Seller",
+                newName: "BirthDay");
+
+            migrationBuilder.RenameColumn(
+                name: "baseSalary",
+                table: "Seller",
+                newName: "BaseSalary");
+
+            migrationBuilder.RenameColumn(
+                name: "Departmentid",
+                table: "Seller",
+                newName: "DepartmentId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Seller_Departmentid",
+                table: "Seller",
+                newName: "IX_Seller_DepartmentId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "Seller",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seller_Department_DepartmentId",
+                table: "Seller",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seller_Department_DepartmentId",
+                table: "Seller");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Seller",
+                newName: "name");
+
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Seller",
+                newName: "email");
+
+            migrationBuilder.RenameColumn(
+                name: "DepartmentId",
+                table: "Seller",
+                newName: "Departmentid");
+
+            migrationBuilder.RenameColumn(
+                name: "BirthDay",
+                table: "Seller",
+                newName: "birthDay");
+
+            migrationBuilder.RenameColumn(
+                name: "BaseSalary",
+                table: "Seller",
+                newName: "baseSalary");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Seller_DepartmentId",
+                table: "Seller",
+                newName: "IX_Seller_Departmentid");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Departmentid",
+                table: "Seller",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seller_Department_Departmentid",
+                table: "Seller",
+                column: "Departmentid",
+                principalTable: "Department",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context; //dependencia  
+        }
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList(); //acessa base de dados vendedores e converte para uma lista. Que vai ser chamada na SellerController
+        }
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove (int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
+
+        
+    }
+}
